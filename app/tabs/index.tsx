3ac1a976ece827352f1ef36ce1a3e866695194fb
MantasJohn <mantasjohnx@gmail.com>
2026-07-08 14:57:56 +0800

    added 3rd number

---
+++ app/tabs/index.tsx
@@ -0,0 +1,30 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About screen
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#3f8291",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#050505",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    color: "#fff",
+  },
+});
